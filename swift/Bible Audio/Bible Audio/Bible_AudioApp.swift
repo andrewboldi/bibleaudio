20bb43edd4fc79e1a313645b43915c499b3d4698
Andrew Boldi <andrewboldi@Andrews-iMac-Pro.local>
2024-07-08 22:52:45 -0700

    add base swift project

---
+++ swift/Bible Audio/Bible Audio/Bible_AudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bible_AudioApp.swift
+//  Bible Audio
+//
+//  Created by Andrew Boldi on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Bible_AudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift/Bible Audio/Bible Audio/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Bible Audio
+//
+//  Created by Andrew Boldi on 7/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
