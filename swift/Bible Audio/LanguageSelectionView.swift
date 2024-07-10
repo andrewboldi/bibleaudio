488202ab08de7c36b4374b413bc5060b0f3a89bc
Andrew Boldi <andrewjboldi@gmail.com>
2024-07-10 16:01:34 -0700

    lol

--- swift/Bible Audio/LanguageSelectionView.swift
+++ swift/Bible Audio/LanguageSelectionView.swift
@@ -2,7 +2,44 @@
 //  LanguageSelectionView.swift
 //  Bible Audio
 //
-//  Created by Andrew Boldi on 7/10/24.
+//  Created by Andrew Boldi on 7/8/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct LanguageSelectionView: View {
+    @AppStorage("selectedLanguage") var selectedLanguage: String?
+    let languages = ["English", "Armenian"]
+    
+    var body: some View {
+        VStack {
+            Text("Select your language")
+                .font(.largeTitle)
+                .padding()
+            
+            Picker(selection: $selectedLanguage, label: Text("Language")) {
+                ForEach(languages, id: \.self) { language in
+                    Text(language).tag(language)
+                }
+            }
+            .pickerStyle(WheelPickerStyle())
+            .padding()
+            
+            if selectedLanguage != nil {
+                Button(action: {
+                    // Action to dismiss this view and proceed
+                }) {
+                    Text("Confirm")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    LanguageSelectionView()
+}
