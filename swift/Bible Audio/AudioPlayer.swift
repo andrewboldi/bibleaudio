7c9d7cca444f5833910df70f6dfd31738f3bd6eb
Andrew Boldi <andrewboldi@andrewmac.local>
2024-07-10 15:15:20 -0700

    test armenian

--- swift/Bible Audio/AudioPlayer.swift
+++ swift/Bible Audio/AudioPlayer.swift
@@ -43,7 +43,7 @@ class AudioPlayer: NSObject, ObservableObject, AVAudioPlayerDelegate {
         // Load your track URLs here. For simplicity, we'll use some dummy URLs.
         // In a real app, you'd fetch these from your server or local storage.
         for i in 0...259 { // 260 chapters in the NT 
-            if let url = URL(string: "https://github.com/Polydynamical/bibleaudio/raw/main/audio/mp3/ESV/chapters/\(String(format: "%03d", arguments: [i])).mp3") {
+            if let url = URL(string: "https://github.com/Polydynamical/bibleaudio/raw/main/audio/mp3/WA2017/chapters/\(String(format: "%03d", arguments: [i])).mp3") {
                 trackURLs.append(url)
             }
         }
